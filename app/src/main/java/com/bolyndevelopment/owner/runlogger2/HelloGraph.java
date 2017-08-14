6efb2b44960b6bb8d4bdeb6d3bf89cf912b41d95
Robert Jones II <robertmjonesii@yahoo.com>
2017-08-13 20:50:18 -0400

    cleaned up non-used code and included new variables to be used in a settings activity

--- app/src/main/java/com/bolyndevelopment/owner/runlogger2/HelloGraph.java
+++ app/src/main/java/com/bolyndevelopment/owner/runlogger2/HelloGraph.java
@@ -5,29 +5,17 @@ import android.databinding.DataBindingUtil;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Gravity;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Spinner;
 
 import com.bolyndevelopment.owner.runlogger2.databinding.ActivityGraphsBinding;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-import java.util.Locale;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -43,33 +31,27 @@ import lecho.lib.hellocharts.model.Line;
 import lecho.lib.hellocharts.model.LineChartData;
 import lecho.lib.hellocharts.model.PointValue;
 import lecho.lib.hellocharts.model.SubcolumnValue;
-import lecho.lib.hellocharts.model.Viewport;
-import lecho.lib.hellocharts.view.ComboLineColumnChartView;
-
-/**
- * Created by Bobby Jones on 8/11/2017.
- */
 
 public class HelloGraph extends AppCompatActivity {
+    //Created by Bobby Jones on 8/11/2017
     public static final String TAG = "HelloGraph";
 
     private static final String LIMIT = " limit ";
-    private static final int VIEWPORT_BASELINE = 0;
-    private static final int VIEWPORT_MAX_HORIZ_OFFSET = 10;
-    private static final int VIEWPORT_HORIZ_OFFSET = 10;
-    private static final int DURATION = 1;
-    private static final int DISTANCE = 2;
-    private static final int CAL_BURNED = 3;
+
     private static final int WEEK = 15;
     private static final int MONTH = 30;
     private static final int YEAR = 100;
 
-    boolean hasRanOnceAlready = false;
+    boolean initialDataLoaded = false;
+    int baseAverage = 3;
+    int zoomLevel = 2;
+    int columnColor = Color.parseColor("#303F9F");
+    int lineColor = Color.parseColor("#00ff00");
+    int axisColor = Color.parseColor("#000000");
 
     int timeFrame = MONTH;
     String query = QueryStrings.DURATION_QUERY;
 
-    int viewportOffset = VIEWPORT_BASELINE;
     Handler handler = new Handler();
     List<Float> rawData;
     List<AxisValue> axisValues = new ArrayList<>();
@@ -82,8 +64,10 @@ public class HelloGraph extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_graphs);
 
-        //handler = new Handler();
-        //axisValues = new ArrayList<>();
+        if (savedInstanceState != null) {
+            timeFrame = savedInstanceState.getInt("timeFrame");
+            query = savedInstanceState.getString("query");
+        }
         initSpinners();
         initGraph();
         initFabs();
@@ -91,7 +75,13 @@ public class HelloGraph extends AppCompatActivity {
         binding.drawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
 
         presentChart(QueryStrings.DURATION_QUERY + LIMIT + timeFrame);
+    }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt("timeFrame", timeFrame);
+        outState.putString("query", query);
+        super.onSaveInstanceState(outState);
     }
 
     private void initSpinners() {
@@ -150,30 +140,22 @@ public class HelloGraph extends AppCompatActivity {
         binding.helloGraph.setValueTouchEnabled(true);
         binding.helloGraph.setOnValueTouchListener(new ValueTouchListener());
         binding.helloGraph.setZoomType(ZoomType.HORIZONTAL);
-
     }
 
     private void initFabs() {
         binding.fabPlus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (viewportOffset <= (VIEWPORT_BASELINE + VIEWPORT_MAX_HORIZ_OFFSET)) {
-                    binding.helloGraph.setZoomLevelWithAnimation(20, 0, 3);
-                    //Viewport viewport = binding.helloGraph.getCurrentViewport();
-                    //viewport.inset(VIEWPORT_HORIZ_OFFSET, 0);
-                    //binding.helloGraph.setCurrentViewportWithAnimation(viewport);
-                    viewportOffset++;
+                if (zoomLevel < 10) {
+                    binding.helloGraph.setZoomLevelWithAnimation(20, 0, zoomLevel++);
                 }
             }
         });
         binding.fabMinus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (viewportOffset >= (VIEWPORT_BASELINE - VIEWPORT_MAX_HORIZ_OFFSET)) {
-                    Viewport viewport = binding.helloGraph.getCurrentViewport();
-                    viewport.inset(-VIEWPORT_HORIZ_OFFSET, 0);
-                    binding.helloGraph.setCurrentViewportWithAnimation(viewport);
-                    viewportOffset--;
+                if (zoomLevel > -1) {
+                    binding.helloGraph.setZoomLevelWithAnimation(20, 0, zoomLevel--);
                 }
             }
         });
@@ -184,18 +166,18 @@ public class HelloGraph extends AppCompatActivity {
             @Override
             public void run() {
                 Cursor c = DatabaseAccess.getInstance().rawQuery(query, null);
-                if (hasRanOnceAlready) {
+                if (initialDataLoaded) {
                     updateColumnData(c);
                 } else {
                     generateColumnData(c);
-                    hasRanOnceAlready = true;
+                    generateLineData();
+                    initialDataLoaded = true;
                 }
-                generateLineData();
                 Axis axis = new Axis(axisValues);
-                axis.setTextColor(Color.BLACK);
+                axis.setTextColor(axisColor);
                 final ComboLineColumnChartData data = new ComboLineColumnChartData(columnData, lineData);
                 data.setAxisXBottom(axis);
-                Axis axisY = new Axis().setHasLines(true).setTextColor(Color.BLACK);
+                Axis axisY = new Axis().setHasLines(true).setTextColor(axisColor);
                 data.setAxisYLeft(axisY);
                 handler.post(new Runnable() {
                     @Override
@@ -209,15 +191,16 @@ public class HelloGraph extends AppCompatActivity {
 
     private void generateColumnData(Cursor results) {
         results.moveToFirst();
-        //int pos = binding.spinnerData.getSelectedItemPosition();
         rawData = new ArrayList<>();
         List<SubcolumnValue> subcolumnValues;
         List<Column> columns = new ArrayList<>();
+        axisValues.clear();
         int count = 0;
         while (!results.isAfterLast()) {
             subcolumnValues = new ArrayList<>();
             final AxisValue av = new AxisValue(count);
-            int mod = count % 3;
+            //we only want to put a label on every third piece of data
+            int mod = count % baseAverage;
             if (count > 0 && mod == 0) {
                 av.setLabel(results.getString(0));
             } else {
@@ -225,8 +208,9 @@ public class HelloGraph extends AppCompatActivity {
             }
             axisValues.add(av);
             float raw = query.equals(QueryStrings.DURATION_QUERY) ? Utils.convertMillisToFloatMinutes(results.getLong(1)) : results.getFloat(1);
+            //we are placing the chart values w/o dates to be used for the average line chart
             rawData.add(raw);
-            subcolumnValues.add(new SubcolumnValue(raw, Color.BLUE));
+            subcolumnValues.add(new SubcolumnValue(raw, columnColor));
             Column col = new Column(subcolumnValues);
             col.setHasLabelsOnlyForSelected(true);
             columns.add(col);
@@ -239,84 +223,56 @@ public class HelloGraph extends AppCompatActivity {
 
     private void updateColumnData(Cursor results) {
         results.moveToFirst();
-        ComboLineColumnChartData oldData = binding.helloGraph.getComboLineColumnChartData();
+        final ComboLineColumnChartData oldData = binding.helloGraph.getComboLineColumnChartData();
         final int cursorCount = results.getCount();
         final int oldColumnCount = oldData.getColumnChartData().getColumns().size();
-        Log.d(TAG, "new: " + cursorCount + ", old: " + oldColumnCount);
+        axisValues.clear();
         int count = 0;
-        int maxValue;
-        boolean cursCountSmaller;
         List<Column> columns = oldData.getColumnChartData().getColumns();
-        Log.d(TAG, "Columns size: " + columns.size());
-        if (cursorCount < oldColumnCount) {
-            maxValue = oldColumnCount;
-            cursCountSmaller = true;
+        if (cursorCount <= oldColumnCount) {
             rawData = new ArrayList<>();
-            List<SubcolumnValue> subcolumnValues;
-
-
             for (int j = 0; j < oldColumnCount; j++) {
                 if (j >= cursorCount) {
                     columns.get(j).getValues().get(0).setTarget(0);
                 } else {
                     float raw = query.equals(QueryStrings.DURATION_QUERY) ? Utils.convertMillisToFloatMinutes(results.getLong(1)) : results.getFloat(1);
                     columns.get(j).getValues().get(0).setTarget(raw);
-                    //subcolumnValues = new ArrayList<>();
+                    rawData.add(raw);
                     final AxisValue av = new AxisValue(count);
-                    int mod = count % 3;
+                    int mod = count % baseAverage;
                     if (count > 0 && mod == 0) {
                         av.setLabel(results.getString(0));
                     } else {
                         av.setLabel("");
                     }
                     axisValues.add(av);
-                    //float raw = query.equals(QueryStrings.DURATION_QUERY) ? Utils.convertMillisToFloatMinutes(results.getLong(1)) : results.getFloat(1);
-                    //rawData.add(raw);
-                    //subcolumnValues.add(new SubcolumnValue(raw, Color.BLUE));
-                    //Column col = new Column(subcolumnValues);
-                    //col.setHasLabelsOnlyForSelected(true);
-                    //columns.add(col);
                     results.moveToNext();
                     count++;
-                    Log.d(TAG, "j: " + j);
                 }
             }
-            //for (int i = oldColumnCount - 1; i >= cursorCount; i--) {
-                //Log.d(TAG, "i: " + i);
-                //columns.remove(i);
-            //}
         } else {
-            maxValue = oldColumnCount;
-            cursCountSmaller = false;
             rawData = new ArrayList<>();
             List<SubcolumnValue> subcolumnValues;
-
             for (int j = 0; j < oldColumnCount; j++) {
                 float raw = query.equals(QueryStrings.DURATION_QUERY) ? Utils.convertMillisToFloatMinutes(results.getLong(1)) : results.getFloat(1);
                 columns.get(j).getValues().get(0).setTarget(raw);
-                //subcolumnValues = new ArrayList<>();
+                rawData.add(raw);
                 final AxisValue av = new AxisValue(count);
-                int mod = count % 3;
+                int mod = count % baseAverage;
                 if (count > 0 && mod == 0) {
                     av.setLabel(results.getString(0));
                 } else {
                     av.setLabel("");
                 }
                 axisValues.add(av);
-                //float raw = query.equals(QueryStrings.DURATION_QUERY) ? Utils.convertMillisToFloatMinutes(results.getLong(1)) : results.getFloat(1);
-                //rawData.add(raw);
-                //subcolumnValues.add(new SubcolumnValue(raw, Color.BLUE));
-                //Column col = new Column(subcolumnValues);
-                //col.setHasLabelsOnlyForSelected(true);
-                //columns.add(col);
                 results.moveToNext();
                 count++;
             }
             for (int i = oldColumnCount; i < cursorCount; i++) {
                 float raw = query.equals(QueryStrings.DURATION_QUERY) ? Utils.convertMillisToFloatMinutes(results.getLong(1)) : results.getFloat(1);
-                //rawData.add(raw);
+                rawData.add(raw);
                 subcolumnValues = new ArrayList<>();
-                subcolumnValues.add(new SubcolumnValue(0, Color.BLUE));
+                subcolumnValues.add(new SubcolumnValue(0, columnColor));
                 subcolumnValues.get(0).setTarget(raw);
                 Column col = new Column(subcolumnValues);
                 col.setHasLabelsOnlyForSelected(true);
@@ -325,7 +281,73 @@ public class HelloGraph extends AppCompatActivity {
                 count++;
             }
         }
-        //columnData = new ColumnChartData(columns);
+        updateLineData(oldColumnCount, cursorCount);
+    }
+
+    private void generateLineData() {
+        List<PointValue> pointValues = new ArrayList<>();
+        List<String> averages = new ArrayList<>();
+        int remainder = rawData.size() % baseAverage;
+        for (int x = 0; x < rawData.size() - remainder; x += 3) {
+            double ave = (rawData.get(x) + rawData.get(x + 1) + rawData.get(x + 2)) / 3;
+            averages.add(String.valueOf(ave));
+        }
+        int index = 1;
+        for (String s : averages) {
+            pointValues.add(new PointValue(index, Float.valueOf(s)));
+            index += 3;
+        }
+        Line line = new Line(pointValues);
+        line.setColor(lineColor);
+        line.setCubic(true);
+        line.setHasLabels(true);
+        line.setHasLines(true);
+        line.setHasPoints(true);
+        List<Line> lines = new ArrayList<>();
+        lines.add(line);
+        lineData = new LineChartData(lines);
+    }
+
+    private void updateLineData(final int oldColumnCount, final int newColumnCount) {
+        List<String> averages = new ArrayList<>();
+        final ComboLineColumnChartData data = binding.helloGraph.getComboLineColumnChartData();
+        List<PointValue> pointValues = data.getLineChartData().getLines().get(0).getValues();
+        final int oldDataCount = pointValues.size();
+        final int remainder = rawData.size() % baseAverage;
+        final int newDataCount = (rawData.size() - remainder) / 3;
+
+        if (newDataCount <= oldDataCount) { //we need to delete the old values
+            for (int x = 0; x < rawData.size() - remainder; x += 3) {
+                double ave = (rawData.get(x) + rawData.get(x + 1) + rawData.get(x + 2)) / 3;
+                averages.add(String.valueOf(ave));
+            }
+            for (int k = 0; k < oldDataCount; k++) {
+                if (k >= newDataCount) {
+                    float x = pointValues.get(k).getX();
+                    pointValues.get(k).setTarget(x, 0);
+                } else {
+                    float x = pointValues.get(k).getX();
+                    pointValues.get(k).setTarget(x, Float.valueOf(averages.get(k)));
+                }
+            }
+        } else { //we need to add in values
+
+            for (int x = 0; x < rawData.size() - remainder; x += 3) {
+                double ave = (rawData.get(x) + rawData.get(x + 1) + rawData.get(x + 2)) / 3;
+                averages.add(String.valueOf(ave));
+            }
+            for (int k = 0; k < oldDataCount; k++) {
+                float x = pointValues.get(k).getX();
+                pointValues.get(k).setTarget(x, Float.valueOf(averages.get(k)));
+            }
+            for (int q = oldDataCount; q < newDataCount; q++) {
+                float xValue = pointValues.get(pointValues.size() - 1).getX();
+                xValue += 3;
+                PointValue pv = new PointValue(xValue, 0);
+                pv.setTarget(xValue, Float.valueOf(averages.get(q)));
+                pointValues.add(pv);
+            }
+        }
         handler.post(new Runnable() {
             @Override
             public void run() {
@@ -337,45 +359,23 @@ public class HelloGraph extends AppCompatActivity {
 
                     @Override
                     public void onAnimationFinished() {
-                        Log.d(TAG, "onAnimationFinished");
                         binding.helloGraph.setDataAnimationListener(null);
-                        if (oldColumnCount > cursorCount) {
-                            for (int i = oldColumnCount - 1; i >= cursorCount; i--) {
-                                Log.d(TAG, "i: " + i);
+                        if (oldColumnCount > newColumnCount) {
+                            for (int i = oldColumnCount - 1; i >= newColumnCount; i--) {
                                 binding.helloGraph.getComboLineColumnChartData().getColumnChartData().getColumns().remove(i);
-                                binding.helloGraph.startDataAnimation(5000);
                             }
                         }
+                        if (oldDataCount > newDataCount) {
+                            for (int e = oldDataCount - 1; e >= newDataCount; e--) {
+                                binding.helloGraph.getComboLineColumnChartData().getLineChartData().getLines().get(0).getValues().remove(e);
+                            }
+                        }
+                        binding.helloGraph.startDataAnimation(2000);
                     }
                 });
                 binding.helloGraph.startDataAnimation(1000);
             }
         });
-
-    }
-
-    private void generateLineData() {
-        List<PointValue> pointValues = new ArrayList<>();
-        List<String> averages = new ArrayList<>();
-        int remainder = rawData.size() % 3;
-        for (int x = 0; x < rawData.size() - remainder; x += 3) {
-            double ave = (rawData.get(x) + rawData.get(x + 1) + rawData.get(x + 2)) / 3;
-            averages.add(String.valueOf(ave));
-        }
-        int index = 1;
-        for (String s : averages) {
-            pointValues.add(new PointValue(index, Float.valueOf(s)));
-            index += 3;
-        }
-        Line line = new Line(pointValues);
-        line.setColor(Color.RED);
-        line.setCubic(true);
-        line.setHasLabels(true);
-        line.setHasLines(true);
-        line.setHasPoints(true);
-        List<Line> lines = new ArrayList<>();
-        lines.add(line);
-        lineData = new LineChartData(lines);
     }
 
     public void onNavClick(View v) {
@@ -411,7 +411,6 @@ public class HelloGraph extends AppCompatActivity {
                 presentChart(query + LIMIT + timeFrame);
             }
         }, 300);
-
     }
 
     private class QueryStrings {
