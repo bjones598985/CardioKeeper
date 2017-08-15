8e121fedb65bd5ed12da4cde75e7b77fdb3590cc
Robert Jones II <robertmjonesii@yahoo.com>
2017-08-15 19:04:50 -0400

    enabled Lap button and recyclerview created landscape version of screen as well **won't to figure out better layout..takes too long to rotate screen

--- app/src/main/java/com/bolyndevelopment/owner/runlogger2/TimerActivity.java
+++ app/src/main/java/com/bolyndevelopment/owner/runlogger2/TimerActivity.java
@@ -4,68 +4,146 @@ import android.databinding.DataBindingUtil;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.bolyndevelopment.owner.runlogger2.databinding.ActivityTimerBinding;
 
-public class TimerActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class TimerActivity extends AppCompatActivity implements View.OnClickListener {
     static final String TAG = TimerActivity.class.getSimpleName();
     ActivityTimerBinding binder;
+    long timeStopped;
+    ArrayList<String> lapList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_timer);
         binder = DataBindingUtil.setContentView(this, R.layout.activity_timer);
-        binder.chronometer.setBase(SystemClock.elapsedRealtime());
+        if (savedInstanceState == null) {
+            binder.chronometer.setBase(SystemClock.elapsedRealtime());
+            lapList = new ArrayList<>();
+        } else {
+            long time = savedInstanceState.getLong("time");
+            binder.chronometer.setBase(SystemClock.elapsedRealtime() + time);
+            this.onClick(binder.startTimer);
+            lapList = savedInstanceState.getStringArrayList("list");
+        }
 
         initButtons();
+        initRv();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putStringArrayList("list", lapList);
+        outState.putLong("time", binder.chronometer.getBase() - SystemClock.elapsedRealtime());
+        super.onSaveInstanceState(outState);
     }
 
     private void initButtons() {
-        binder.startTimer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        binder.startTimer.setOnClickListener(this);
+        binder.stopTimer.setOnClickListener(this);
+        binder.lap.setOnClickListener(this);
+        binder.resumeTimer.setOnClickListener(this);
+        binder.resetTimer.setOnClickListener(this);
+    }
+
+    private void initRv() {
+        binder.list.setHasFixedSize(true);
+        binder.list.setLayoutManager(new LinearLayoutManager(this));
+        binder.list.setAdapter(new LapAdapter());
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.start_timer:
                 binder.chronometer.start();
-            }
-        });
-        binder.stopTimer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                binder.startTimer.animate().alpha(0).start();
+                binder.startTimer.setVisibility(View.INVISIBLE);
+                binder.stopTimer.animate().alpha(1f).start();
+                binder.stopTimer.setVisibility(View.VISIBLE);
+                binder.lap.animate().alpha(1f).start();
+                binder.lap.setVisibility(View.VISIBLE);
+                break;
+
+            case R.id.stop_timer:
+                timeStopped = binder.chronometer.getBase() - SystemClock.elapsedRealtime();
                 binder.chronometer.stop();
-            }
-        });
-        binder.lap.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d(TAG, "Time: " + binder.chronometer.getText());
-            }
-        });
+                binder.stopTimer.animate().alpha(0f).start();
+                binder.stopTimer.setVisibility(View.INVISIBLE);
+                binder.lap.animate().alpha(0f).start();
+                binder.lap.setVisibility(View.INVISIBLE);
+                binder.resumeTimer.animate().alpha(1f).start();
+                binder.resumeTimer.setVisibility(View.VISIBLE);
+                binder.resetTimer.animate().alpha(1f).start();
+                binder.resetTimer.setVisibility(View.VISIBLE);
+                break;
+            case R.id.lap:
+                String time = binder.chronometer.getText().toString();
+                lapList.add(0, time);
+                binder.list.getAdapter().notifyItemInserted(0);
+                binder.list.scrollToPosition(0);
+                break;
+            case R.id.resume_timer:
+                binder.chronometer.setBase(SystemClock.elapsedRealtime() + timeStopped);
+                binder.chronometer.start();
+                binder.resumeTimer.animate().alpha(0f).start();
+                binder.resumeTimer.setVisibility(View.INVISIBLE);
+                binder.resetTimer.animate().alpha(0f).start();
+                binder.resetTimer.setVisibility(View.INVISIBLE);
+                binder.stopTimer.animate().alpha(1f).start();
+                binder.stopTimer.setVisibility(View.VISIBLE);
+                binder.lap.animate().alpha(1f).start();
+                binder.lap.setVisibility(View.VISIBLE);
+                break;
+            case R.id.reset_timer:
+                binder.chronometer.setBase(SystemClock.elapsedRealtime());
+                timeStopped = 0;
+                binder.resumeTimer.animate().alpha(0f).start();
+                binder.resumeTimer.setVisibility(View.INVISIBLE);
+                binder.resetTimer.animate().alpha(0f).start();
+                binder.resetTimer.setVisibility(View.INVISIBLE);
+                binder.chronometer.setBase(SystemClock.elapsedRealtime());
+                binder.startTimer.animate().alpha(1f).start();
+                binder.startTimer.setVisibility(View.VISIBLE);
+        }
     }
 
     private class LapAdapter extends RecyclerView.Adapter<LapAdapter.LapHolder> {
+        int count = 0;
 
         @Override
         public LapHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            return null;
+            final View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+            return new LapHolder(view);
         }
 
         @Override
         public void onBindViewHolder(LapHolder holder, int position) {
-
+            holder.order.setText(String.valueOf(lapList.size() - position));
+            holder.lap.setText(lapList.get(position));
         }
 
         @Override
         public int getItemCount() {
-            return 0;
+            return lapList.size();
         }
 
         class LapHolder extends RecyclerView.ViewHolder {
+            TextView order, lap;
             public LapHolder(View itemView) {
                 super(itemView);
+                order = (TextView) itemView.findViewById(R.id.order_textview);
+                lap = (TextView) itemView.findViewById(R.id.lap_info);
             }
         }
     }
