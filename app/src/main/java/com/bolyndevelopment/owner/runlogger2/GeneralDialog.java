7f46dca3870d758f31216d43259fd3ffc9710944
Robert Jones II <robertmjonesii@yahoo.com>
2017-10-29 09:08:14 -0400

    clean general dialog code

--- app/src/main/java/com/bolyndevelopment/owner/runlogger2/GeneralDialog.java
+++ app/src/main/java/com/bolyndevelopment/owner/runlogger2/GeneralDialog.java
@@ -6,28 +6,18 @@ import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.DataSource;
-import com.bumptech.glide.load.engine.GlideException;
-import com.bumptech.glide.request.RequestListener;
-import com.bumptech.glide.request.target.Target;
 
-/**
- * Created by Bobby Jones on 9/27/2017.
- */
+//Created 9/27/2017.
 
 public class GeneralDialog extends DialogFragment {
-    static final String TAG = "GeneralDialog";
     View root;
 
     private GeneralDialogListener listener;
@@ -36,6 +26,7 @@ public class GeneralDialog extends DialogFragment {
         void onGeneralDialogButtonClicked(int buttonId);
     }
 
+    @SuppressLint("InflateParams")
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,6 +43,7 @@ public class GeneralDialog extends DialogFragment {
         }
     }
 
+    @SuppressWarnings("ConstantConditions")
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
